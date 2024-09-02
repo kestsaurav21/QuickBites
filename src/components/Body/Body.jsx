276be1416b2ae261a1892a6f825486600f41bbd0
kestsaurav21 <kestsaurav21@outlook.com>
2024-09-02 21:52:50 +0530

    Header

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SearchBar from '../SearchBar/SearchBar'
+
+const Body = () => {
+  return (
+    <div >
+        <SearchBar/>
+        
+
+    </div>
+  )
+}
+
+export default Body
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,19 +10,19 @@ const Header = () => {
 const [btn, setBtn] = useState("Login");
 
   return (
-    <div className='flex justify-between'>
+    <div className='flex justify-between 	'>
         <div className='cursor-pointer'>
             <img className='w-40'
             src={logo} alt="logo" />
         </div>
         <div className='flex items-center px-4'>
-            <ul className='flex space-x-4'>
-                <li className='px-8 cursor-pointer'>Home</li>
-                <li className='px-8 cursor-pointer'>About</li>
-                <li className='px-8 cursor-pointer'>Contact</li>
-                <li className='px-8 cursor-pointer'>Cart</li>
+            <ul className='flex space-x-10'>
+                <li className='px-8 m-4 cursor-pointer'>Home</li>
+                <li className='px-8 m-4 cursor-pointer'>About</li>
+                <li className='px-8 m-4 cursor-pointer'>Contact</li>
+                <li className='px-8 m-4 cursor-pointer'>Cart</li>
 
-                <button className='px-6  bg-orange-600 text-white font-bold rounded-xl cursor-pointer'
+                <button className='px-8  bg-orange-600 text-white font-bold rounded-xl cursor-pointer'
                 onClick={() => {
                     btn === 'Login' 
                     ? setBtn("Logout") 
