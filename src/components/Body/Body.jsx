62f657fcdc335e1dfc8a77ce35cc37be2b1bf038
kestsaurav21 <kestsaurav21@outlook.com>
2024-09-03 01:24:04 +0530

    Restaurant Card Component

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SearchBar from '../SearchBar/SearchBar'
+import { SWIGGY_API } from '../../utils/constants';
+import RestaurantCard from '../RestaurantCard/RestaurantCard';
 
 const Body = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(()=> {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const response = await fetch(SWIGGY_API);
+        const json = await response.json();
+        
+        const resData = json.data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        setData(resData);
+    }
+    
+    
+    
+
   return (
     <div >
         <SearchBar/>
-        
+        {
+            data?.map(item => <RestaurantCard resData={item} />)
+        }
 
+        {/* {
+            data?.map(item => <p>{item}</p>)
+        } */}
     </div>
   )
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const SWIGGY_API =
+    "https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=28.4089123&lng=77.3177894&carousel=true&third_party_vendor=1"
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/";
