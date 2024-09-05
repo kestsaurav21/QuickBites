1961444852be5c9a2320699282e662de333ed6c6
kestsaurav21 <kestsaurav21@outlook.com>
2024-09-05 17:41:43 +0530

    feat: sort menu on rating

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -4,6 +4,7 @@ import MenuCard from '../MenuCard/MenuCard';
 const Body = ({ menuData }) => {
   const [value, setValue] = useState("");
   const [data, setData] = useState(menuData);
+  // const [filter, setFilter] = useState("");
   
   
     function handleChange (e){
@@ -16,12 +17,34 @@ const Body = ({ menuData }) => {
       if(!value) setData(menuData);
 
       const arr = menuData.filter((item) =>
-        item.name.includes(value)
+        item.name.toLowerCase().includes(value.toLowerCase())
       )
       setData(arr);
       
       
     }
+
+    function handleFeatured(e){
+
+      console.log(e.target.value);
+      
+      if(e.target.value === "Featured") setData(menuData);
+
+      else if (e.target.value === "Top Rated"){
+        const arr = menuData?.filter((item) => 
+          item.rate > 4 
+        )
+        setData(arr)
+      }
+      else if(e.target.value === "Low Price"){
+        const sortArr = menuData.sort((a,b)=> a.price - b.price);
+        setData(sortArr);
+
+      }
+      
+     
+    }
+    
 
 
   return (
@@ -35,7 +58,13 @@ const Body = ({ menuData }) => {
         <button 
         onClick={()=> handleSearch()}
         className='px-4 py-2 m-4 rounded-xl bg-black text-white'>Search</button>
-        <button className='px-4 py-2 m-4 rounded-xl bg-black text-white'>Top Rated</button>
+        <select 
+        onChange={(e)=> handleFeatured(e)}
+        className='px-4 py-2 m-4 rounded-xl bg-black text-white'>
+        <option value="Featured">Featured</option>
+        <option value="Top Rated">Top Rated</option>
+        <option value="Low Price">Low Price</option>
+        </select>
         </div>
 
         <div className='flex flex-wrap justify-evenly'>
